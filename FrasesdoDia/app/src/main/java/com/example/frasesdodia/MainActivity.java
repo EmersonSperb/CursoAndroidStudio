aa7e94bda143d2e6ee88aceca5639df01276b73a
Emerson Sperb <emersonsperb1983@gmail.com>
2020-09-28 11:45:20 -0300

    Códigos no aplicativo das frases

--- FrasesdoDia/app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ FrasesdoDia/app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+
+        String[] frases = {"Frase 0",
+                           "Frase 1",
+                           "Frase 2",
+                           "Frase 3",
+                           "Frase 4",
+                           "Frase 5"}
+        ;
+
+        int indice = new Random().nextInt(frases.length);//(bound: 5)
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText( frases[indice] );
+
+
     }
 }
