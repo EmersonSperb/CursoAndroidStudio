6da30155dc37adc910c5b6045a396f1781c74bf3
Emerson Sperb <emersonsperb1983@gmail.com>
2020-09-30 15:49:37 -0300

    Álcool ou gasolina Layout

--- Alcoolougasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ Alcoolougasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcular(View view) {
+        double valorAlcool,valorGasolina,valorResultado;
+        String melhorCombustivel;
+
+        TextInputEditText precoAlcool = findViewById(R.id.textPrecoAlcool);
+        TextInputEditText precoGasolina = findViewById(R.id.textPrecoGasolina);
+
     }
 }
