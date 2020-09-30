ff45f4e493e495ce2fee751fe1ffc43fcd09dc33
Emerson Sperb <emersonsperb1983@gmail.com>
2020-09-30 17:13:49 -0300

    Álcool ou gasolina - Cálculos

--- Alcoolougasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ Alcoolougasolina/app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -4,23 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputEditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool   = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
     }
 
-    public void calcular(View view) {
-        double valorAlcool,valorGasolina,valorResultado;
-        String melhorCombustivel;
 
-        TextInputEditText precoAlcool = findViewById(R.id.textPrecoAlcool);
-        TextInputEditText precoGasolina = findViewById(R.id.textPrecoGasolina);
+    public void calcularPreco(View view) {
+        //recuperar valores digitados
+        String precoAlcool   = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+       //Validar os campos digitados
+        boolean camposValidados = validarCampos(precoAlcool,precoGasolina);
 
+        if ( camposValidados ) {
+        //Converte String para Double
+            Double valorAlcool = Double.parseDouble( precoAlcool );
+            Double valorGasolina = Double.parseDouble( precoGasolina );
+            /* Fazer cálculo de menor preço
+             * Se (valorAlcool / valorGasolina) >= 0.7 é melhor utilizar gasolina
+             *   senão é melhor utilizar álcool
+             * */
+            Double resultado = valorAlcool / valorGasolina;
+            if (resultado >= 0.7){
+                textResultado.setText("Melhor usar Gasolina");
+            }else {
+                textResultado.setText("Melhor usar Álcool");};
+        }else{
+            textResultado.setText("Preencha os campos primeiro.");
+        }
     }
+
+    public boolean validarCampos(String pAlcool, String pGasolina) {
+        boolean camposValidados = true;
+        if (pAlcool == null || pAlcool.equals("") ){
+            camposValidados = false;
+        }else if (pGasolina == null || pGasolina.equals("") ) {
+            camposValidados = false;
+        };
+        return camposValidados;
+    }
+
 }
+
