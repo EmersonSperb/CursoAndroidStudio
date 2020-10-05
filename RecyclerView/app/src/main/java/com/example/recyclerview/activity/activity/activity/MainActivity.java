1f1bac4ff3640529a0f2c1231293254cc76b908e
Emerson Sperb <emersonsperb1983@gmail.com>
2020-10-05 11:56:40 -0300

    Recycler View

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.recyclerview.activity.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.activity.activity.adapter.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        //Configurar adapter
+        Adapter adapter = new Adapter();
+        recyclerView.setHasFixedSize(true);
+        //Configurar Recycler View
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
