4d4b22dbec10a3e57ffc8a47aa83085949ea4b59
Emerson Sperb <emersonsperb1983@gmail.com>
2020-10-28 17:45:20 -0300

    Organizze Toolbar

--- Calendario/app/src/main/java/com/example/calendario/MainActivity.java
+++ Calendario/app/src/main/java/com/example/calendario/MainActivity.java
@@ -1,18 +1,60 @@
 package com.example.calendario;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class MainActivity extends AppCompatActivity {
-    private CalendarView calendarView;
+    //private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         calendarView = findViewById(R.id.calendarView);
+        /*calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2016,1,1))
+                .setMaximumDate(CalendarDay.from(2022,12,31))
+        .commit();*/
+        CharSequence meses[] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+        CharSequence dias[] = {"Dom","Seg","Ter","Qua","Qui","Sex","Sáb"};
+        calendarView.setWeekDayLabels(dias);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data","valor: " + date);
+                Log.i("data","valor: " + date.getDay() + "/" + (date.getMonth() + 1) + "/" + date.getYear());
+            }
+        });
+
+        /*calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Log.i("data","valor: " +date);
+
+            }
+        });*/
+
+        /*calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.i("data","valor: " + year + "/" + month + "/" + dayOfMonth);
+            }
+        });*/
+
     }
+
+
 }
--- Organizze/app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
+++ Organizze/app/src/main/java/com/example/organizze/activity/PrincipalActivity.java
@@ -3,24 +3,53 @@ package com.example.organizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.organizze.config.ConfiguracaoFirebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.organizze.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+    private MaterialCalendarView calendarView;
+    private RecyclerView recyclerMovimentos;
+    private TextView textSaudacao;
+    private TextView textSaldo;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
+
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
+        recyclerMovimentos = findViewById(R.id.recyclerMovimentos);
+        textSaudacao = findViewById(R.id.textSaudacao);
+        textSaldo = findViewById(R.id.textSaldo);
+
+        /*calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2016,1,1))
+                .setMaximumDate(CalendarDay.from(2022,12,31))
+        .commit();*/
 
         /*FloatingActionButton fab = findViewById(R.id.menu);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,5 +68,37 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarReceita (View view){
         startActivity(new Intent(this,ReceitasActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case  R.id.menuSair :
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void configuraCalendarView (){
+        CharSequence meses[] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data","valor: " + date);
+                Log.i("data","valor: " + date.getDay() + "/" + (date.getMonth() + 1) + "/" + date.getYear());
+            }
+        });
     }
 }
